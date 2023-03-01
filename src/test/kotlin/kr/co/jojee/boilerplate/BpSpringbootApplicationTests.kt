c95aeadd080bba4d56fbbf1028e28ca6e7e65352
Jo-Jee <39206510+Jo-Jee@users.noreply.github.com>
2023-03-01 16:40:15 +0900

    Initial commit

---
+++ src/test/kotlin/kr/co/jojee/boilerplate/BpSpringbootApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.jojee.boilerplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BpSpringbootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
